b2c51a186249d30cda8c14889c2de37fe6cbec47
prerna <prerna001>
2020-02-25 09:24:42 -0500

    Lab 2

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,36 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    private final String port;
+    private final String memoryLimit;
+    private final String cfInstanceIndex;
+    private String cfInstanceAddr;
+
+    public EnvController(@Value("${port:NOT SET}") String port, @Value("${memoryLimit:NOT SET}") String memoryLimit, @Value("${cfInstanceIndex:NOT SET}") String cfInstanceIndex,  @Value("${cfInstanceAddr:NOT SET}") String cfInstanceAddr){
+        this.port = port;
+        this.memoryLimit = memoryLimit;
+        this.cfInstanceIndex = cfInstanceIndex;
+        this.cfInstanceAddr = cfInstanceAddr;
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv() {
+        Map<String, String> env = new HashMap<>();
+
+        env.put("PORT", port);
+        env.put("MEMORY_LIMIT", memoryLimit);
+        env.put("CF_INSTANCE_INDEX", cfInstanceIndex);
+        env.put("CF_INSTANCE_ADDR", cfInstanceAddr);
+
+        return env;
+    }
+}
--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    private String message;
+
+    public WelcomeController(@Value("${welcome.message}") String welcomeMessage){
+        this.message = welcomeMessage;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return message;
     }
 }
